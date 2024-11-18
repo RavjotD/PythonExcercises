dd8626317a2022d83f9ca9ae95102d74403de6b9
Ravjot Duhra <duhraravjot@gmail.com>
2024-11-17 18:17:41 -0800

    updated test cases

--- SelectionSort/TestSelectionSort.py
+++ SelectionSort/TestSelectionSort.py
@@ -7,12 +7,11 @@ from SelectionSort import SelectionSort
 class TestSelectionSort(unittest.TestCase):
     def testing_cases(self):
         self.assertEqual(SelectionSort([0,1,0,3,12]),([0,0,1,3,12]))
-        self.assertEqual(SelectionSort([]),([]))
-        self.assertEqual(SelectionSort([]),([]))
-        self.assertEqual(SelectionSort([]),([]))
-        self.assertEqual(SelectionSort([]),([]))
-        self.assertEqual(SelectionSort([]),([]))
-        self.assertEqual(SelectionSort([]),([]))
+        self.assertEqual(SelectionSort([3,1,5,2,11]),([1,2,3,5,11]))
+        self.assertEqual(SelectionSort([4,3,1,2]),([1,2,3,4]))
+        self.assertEqual(SelectionSort([3,2,4]),([2,3,4]))
+        self.assertEqual(SelectionSort([1,0,3,5,4]),([0,1,3,4,5]))
+      
         
 if __name__ == '__main__':
     unittest.main()
